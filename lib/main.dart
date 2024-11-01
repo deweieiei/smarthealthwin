478ad146b5b851d189bccc5ccf6efcb32154d458
dew <babydew2015@gmail.com>
2024-11-01 16:07:05 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:smarthealthwin/myapp/myapp.dart';
-import 'package:smarthealthwin/station/app/utils/global_http_overrides.dart';
 
 void main() {
-  HttpOverrides.global = GlobalHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
