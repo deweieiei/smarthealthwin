bd856764218b0922c2fdca1a45844487c6091b16
dew <babydew2015@gmail.com>
2024-11-01 16:21:36 +0700

    up

---
+++ lib/telemadwin/provider/provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/widgets.dart';
+
+class Provider with ChangeNotifier {
+  Map userinformation = {};
+  Map setting = {
+    "platfromURL": "",
+    "": "",
+  };
+}
